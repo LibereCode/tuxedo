d7a9ed2ec44a45a9d855470961910392ed367e0e
LibereCode <libere-code@posteo.org>
2026-09-13 20:15:04 +0200

    feat(dev): devenv

---
+++ devenv.nix
@@ -0,0 +1,57 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+        mise
+  ];
+
+  languages = {
+    rust = {
+      enable = true;
+      components = [
+        "rustc"
+        "cargo"
+        "clippy"
+        "rustfmt"
+        "rust-analyzer"
+      ];
+    };
+    nix.enable = true;
+  };
+
+  processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+  enterShell = ''
+    hello         # Run scripts directly
+    git --version # Use packages
+  '';
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
